e73a012dbb6fa359734dcfa5b1ce91c0a12ce040
Igor Luppi <igor.luppi@gmail.com>
2023-05-27 14:53:19 -0300

    added example and script

--- script.sh
+++ script.sh
@@ -4,6 +4,8 @@ while IFS=',' read -r col1 col2 col3 col4 || [[ -n $col1 ]]
 do
     if [[ -n $col1 ]]; then
         # Process the non-empty line
-        echo "Column 1: $col1, Column 2: $col2, Column 3: $col3, Column 4: $col4"
+        echo "Input: $col1, Begin: $col2, End: $col3, Output: $col4"
+        # example: $col1 = IN.mp4, $col2 = 00:30:00, $col3 = 01:45:00, $col4 = OUT.mp4
+        melt $col1 in=$col2 out=$col3 -consumer avformat:$col4
     fi
 done < $1
